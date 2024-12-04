6798470ef80a71f7b81246b3c700e993bc82d2e3
jdbenitop <167857170+jdbenitop@users.noreply.github.com>
2024-12-04 09:34:10 +0100

    Create favorits.js

---
+++ Screens/favorits.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const FavoritesScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Favorites</Text>
+      {/* Aquí afegiries el llistat de vídeos favorits */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20 },
+  title: { fontSize: 24, fontWeight: 'bold' },
+});
+
+export default FavoritesScreen;
